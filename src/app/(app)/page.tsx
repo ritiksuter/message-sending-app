1956d2bbc1472753afdf7a829df7dcf6a1a17d60
Ritik Suteri <ritiksuteri@gmail.com>
2024-10-19 12:43:25 +0530

    first commit on message-app

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import * as React from "react"
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import AutoPlay from 'embla-carousel-autoplay';
+import messages from '@/messages.json'
+
+function Home() {
+  return (
+    <>
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl">Dive into the World of Anonymous Conversations</h1>
+          <p className="mt-3 md:mt-4 text-base">Explore Mystry Message - Where your identity remains a secret.</p>
+        </section>
+        <Carousel plugins={[AutoPlay({ delay: 2000 })]} className="w-full max-w-xs">
+          <CarouselContent>
+            {
+              messages.map((message, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardHeader>{message.title}</CardHeader>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-lg font-semibold">{message.content}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))
+            }
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </main>
+      <footer className="text-center p-4 md:p-6">2023 Mystry Message. All rights reserved.</footer>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,36 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const { username, code } = await request.json();
+
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({ username: decodedUsername });
+
+        if(!user) {
+            return Response.json({ success: false, message: "User not found" }, { status: 400 });
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+            return Response.json({ success: true, message: "Account verified successfully" }, { status: 200 });
+        }
+        else if(!isCodeNotExpired) {
+            return Response.json({ success: false, message: "Verification Code Expired, please signup again to get new Verification Code" }, { status: 400 });
+        }
+        else if(!isCodeValid) {
+            return Response.json({ success: false, message:"Incorrect verification code" }, { status: 400 });
+        }
+    }
+    catch (error) {
+        console.error("Error in Verifying User", error);
+        return Response.json({ success: false, message: "Error in verifying User" }, { status: 500 });
+    }
+}
